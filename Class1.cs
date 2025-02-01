fbf5b6106626cf5f6f053ee699b7e2d338e2439d
Waidelos <psykhokdizxc@gmail.com>
2025-02-02 00:43:24 +0700

    mod init

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace WaidFirstMod
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("Марина привет я мод я тебя люблю йоу!", LogLevel.Info);
+        }
+    }
+}
